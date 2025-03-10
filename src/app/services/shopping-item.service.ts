7efe874398b616e48bd8fd65488b9782cd9c2646
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-03-09 23:27:48 -0400

    Buttons, labels, inputs, lists, alerts and services

---
+++ src/app/services/shopping-item.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingItemService {
+
+  public items: string[];
+  public isEmpty: boolean;
+  
+  constructor() { 
+    this.items = [];
+    this.isEmpty = true;
+    
+  }
+
+  addItem(item: string){
+    this.items.push(item);
+    this.isEmpty = false;
+  }
+
+  removeItema(item: string){
+    let index = this.items.findIndex(it => it.toUpperCase().trim() === item.toUpperCase());
+    if(index != -1){
+      this.items.splice(index, 1); //delete the array position
+      if(this.items.length == 0){
+        this.isEmpty = true;
+      }
+    }
+  }
+
+  removeAllItem(){
+    this.items = [];
+    this.isEmpty = true;
+  }
+
+  existsItem(item: string){
+    const itemFound = this.items.find(it => it.toUpperCase().trim() === item.toUpperCase()); //gives you all elements
+    return itemFound;
+  }
+}
