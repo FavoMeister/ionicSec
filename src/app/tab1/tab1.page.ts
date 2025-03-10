319c99ab8ca5ddae82b982ec920dd320f4edf2e7
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-03-09 23:40:29 -0400

    Changes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShoppingItemService } from '../services/shopping-item.service';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,8 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public shoppingList: ShoppingItemService
+  ) {}
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ShoppingItemService } from '../services/shopping-item.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,7 +9,41 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab2Page {
+  public item: string;
+  
+  constructor(
+    private shoppingList: ShoppingItemService,//To inject a service
+    private alertController: AlertController
+  ) {}
+  
+  addItem(){
+    console.log(this.item);
 
-  constructor() {}
+    if(!this.shoppingList.existsItem(this.item)){
+      this.shoppingList.addItem(this.item);
+      this.item = '';
+      console.log(this.shoppingList.items);
+      this.alertSuccess();
+    }else{
+      this.alertError();
+    }
+  }
 
+  async alertSuccess(){
+    const alert = await this.alertController.create({
+      header: "Success",
+      message: '¡Item Added!',
+      buttons: ['OK']
+    })
+    await alert.present();
+  }
+
+  async alertError(){
+    const alert = await this.alertController.create({
+      header: "Error",
+      message: '¡The Item already exists!',
+      buttons: ['OK']
+    })
+    await alert.present();
+  }
 }
